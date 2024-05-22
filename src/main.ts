586079bab8c80b06292438df9cccbc6d511bdd52
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2024-05-22 14:14:20 +0200

    add swagger to API
    add & init swagger to API
    update README.md & add the link to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Outil_veille_API')
+    .setDescription(
+      'API de récupération de différentes sources de veille techniques',
+    )
+    .setVersion('1.0')
+    .addTag('veille')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
